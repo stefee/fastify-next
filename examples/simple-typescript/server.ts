8bc2b1c61c8f774b97b1919d2fc34fd11042a95c
Stef <git@sril.email>
2022-11-06 10:05:19 +0000

    Update deps

--- examples/simple-typescript/server.ts
+++ examples/simple-typescript/server.ts
@@ -5,7 +5,7 @@ async function init() {
   const fastify = Fastify({ logger: true, disableRequestLogging: true });
   await fastify.register(FastifyNext);
   fastify.next("*");
-  await fastify.listen({ port: 3000, host: "localhost" });
+  await fastify.listen({ port: 3000, host: "127.0.0.1" });
 }
 
 init().catch((error) => {
